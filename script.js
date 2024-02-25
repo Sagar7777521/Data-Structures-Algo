883a81593d3bb2613c153aca0b257027116dbc01
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-02-25 16:21:52 +0530

    Binary Search Recusrsive

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function recurfact(n) {
-  if (n === 0) {
-    return 1;
+function binarySearch(arr, target) {
+  let leftIndex = 0;
+  let rightIndex = arr.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+    if (target === arr[middleIndex]) {
+      return middleIndex;
+    }
+    if (target < arr[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
   }
-  return n * recurfact(n - 1);
+  return -1;
 }
-console.log(recurfact(5));
+console.log(binarySearch([2, 3, 5, 6, 8, 9, 11, 22, 44], 11));
+console.log(binarySearch([2, 3, 5, 6, 8, 9, 11, 22, 44], 1));
+console.log(binarySearch([2, 3, 5, 6, 8, 9, 11, 22, 44], 211));
