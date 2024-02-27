82c4b0970a2c39ef5b71eb331519b7c859c98f2b
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-02-27 17:35:18 +0530

    Quick Sort

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-function binarySearch(arr, target) {
-  let leftIndex = 0;
-  let rightIndex = arr.length - 1;
+// ************ INSERTION SORT**************
 
-  while (leftIndex <= rightIndex) {
-    let middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-    if (target === arr[middleIndex]) {
-      return middleIndex;
-    }
-    if (target < arr[middleIndex]) {
-      rightIndex = middleIndex - 1;
+// function insertionSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     let numberToInsert = arr[i];
+//     let j = i - 1;
+//     while (j >= 0 && arr[j] > numberToInsert) {
+//       arr[j + 1] = arr[j];
+//       j = j - 1;
+//     }
+//     arr[j + 1] = numberToInsert;
+//   }
+// }
+
+// const arr = [-8, 44, 22, -2, -10, -22, 33, 24, 65, 34, 78];
+// insertionSort(arr);
+// console.log("Insertion Sorted Array is", arr);
+// **************** QUICK SORT ******************
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
     } else {
-      leftIndex = middleIndex + 1;
+      right.push(arr[i]);
     }
   }
-  return -1;
+  return [...quickSort(left), pivot, ...quickSort(right)];
 }
-console.log(binarySearch([2, 3, 5, 6, 8, 9, 11, 22, 44], 11));
-console.log(binarySearch([2, 3, 5, 6, 8, 9, 11, 22, 44], 1));
-console.log(binarySearch([2, 3, 5, 6, 8, 9, 11, 22, 44], 211));
+
+const arr = [-2, 3, -42, -64634, 33, 55, 77, 99, 12];
+
+console.log("Quick Sort array is", quickSort(arr));
+
+// ***************** MERGE SORT **********************
