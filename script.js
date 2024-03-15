259db55203e96914e37e98b64601a8e6beb9df30
Sagar <70952463+Sagar7777521@users.noreply.github.com>
2024-03-15 18:11:53 +0530

    Done with Misc Problems

--- script.js
+++ script.js
@@ -15,26 +15,70 @@
 // const arr = [-8, 44, 22, -2, -10, -22, 33, 24, 65, 34, 78];
 // insertionSort(arr);
 // console.log("Insertion Sorted Array is", arr);
+
 // **************** QUICK SORT ******************
-function quickSort(arr) {
-  if (arr.length < 2) {
-    return arr;
-  }
-  let pivot = arr[arr.length - 1];
-  let left = [];
-  let right = [];
-  for (let i = 0; i < arr.length - 1; i++) {
-    if (arr[i] < pivot) {
-      left.push(arr[i]);
-    } else {
-      right.push(arr[i]);
-    }
-  }
-  return [...quickSort(left), pivot, ...quickSort(right)];
-}
 
-const arr = [-2, 3, -42, -64634, 33, 55, 77, 99, 12];
+// function quickSort(arr) {
+//   if (arr.length < 2) {
+//     return arr;
+//   }
+//   let pivot = arr[arr.length - 1];
+//   let left = [];
+//   let right = [];
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     if (arr[i] < pivot) {
+//       left.push(arr[i]);
+//     } else {
+//       right.push(arr[i]);
+//     }
+//   }
+//   return [...quickSort(left), pivot, ...quickSort(right)];
+// }
 
-console.log("Quick Sort array is", quickSort(arr));
+// const arr = [-2, 3, -42, -64634, 33, 55, 77, 99, 12];
+
+// console.log("Quick Sort array is", quickSort(arr));
 
 // ***************** MERGE SORT **********************
+
+// *******************Cartesian Product**********************
+
+function cartesianProduct(arr1, arr2) {
+  const result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; i < arr2.length; j++) {
+      result.push([arr1[i], arr2[j]]);
+    }
+  }
+  return result;
+}
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6, 7];
+console.log("Cartesian Product is", cartesianProduct(arr1, arr2));
+
+// ******************* Climbing Staircase Problem **********************
+
+function climbingStairCase(n) {
+  const noOfWays = [1, 2];
+  for (let i = 2; i <= n; i++) {
+    noOfWays[i] = noOfWays[i - 1] + noOfWays[i - 2];
+  }
+  return noOfWays[n - 1];
+}
+
+console.log("Number of Ways are", climbingStairCase(5));
+
+// ********************* Tower of Hanoi ************************
+
+function towerOfHanoi(n, fromRod, toRod, usingRod) {
+  if (n === 1) {
+    console.log(`Move disk 1 from ${fromRod} to ${toRod}`);
+    return;
+  }
+  towerOfHanoi(n - 1, fromRod, usingRod, toRod);
+  console.log(`Move disk ${n} from ${fromRod} to ${toRod}`);
+  towerOfHanoi(n - 1, usingRod, toRod, fromRod);
+}
+
+towerOfHanoi(3, "A", "C", "B");
